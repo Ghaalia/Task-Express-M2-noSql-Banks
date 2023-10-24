0612627cd640d6a9ecc249258a7b80c7564aeefb
Ghaalia <Ghaliaals9@gmail.com>
2023-10-24 18:35:32 +0300

    ..

---
+++ database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(
+      "mongodb+srv://Ghaalia:OxvSvpq94oGxTR7w@cluster0.mx8vcfj.mongodb.net/FirstDB"
+    );
+    console.log(`mongo connected: ${con.connection.host}`);
+  } catch {
+    console.log(`Somthing went wrng while connecting to DataBase `);
+  }
+};
+
+module.exports = connectDB;
